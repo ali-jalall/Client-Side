d1c6b7f7d1a18429b7db9148224f26630067169a
alijalal-sys <ajmabilal@gmail.com>
2020-03-21 19:31:37 +0100

    Home Page

---
+++ src/pages/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="main">
+    <NavBar />
+    <Hero />
+    <Features />
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar";
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+    Hero,
+    Features
+  }
+};
+</script>
+<style scoped>
+#main {
+  overflow-x: hidden;
+}
+</style>

3ca3edd27749d6efc3748edf1c12a1f9292e88a0
alijalal-sys <ajmabilal@gmail.com>
2020-03-21 19:31:27 +0100

    cleaning code and setting up bootstrap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "./router/index";
+import { BootstrapVue } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')

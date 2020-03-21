a645b61e01d8c3ab60fe443d16fee941befcf820
alijalal-sys <ajmabilal@gmail.com>
2020-03-21 19:32:02 +0100

    Features component

---
+++ src/components/Features.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+      <h2 class="text-center mt-5">
+        <strong>Why You Should Choose us</strong>
+      </h2>
+      <hr style="background-color: #8BE2BC; padding: 2px 0px; width: 100px;" />
+      <b-row class="text-center mt-5">
+        <b-col lg="5">
+          <div
+            class="m-auto"
+            style="border: 2px solid black; height: 500px; width: 400px "
+          ></div>
+        </b-col>
+        <b-col lg="7">
+          <b-row style="font-size: 20pt; margin: 3% 0px 7%">
+            <b-col lg="1"><i class="fas fa-shopping-cart fas-10x fa-2x"> </i></b-col>
+            <b-col lg="11">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+            </b-col>
+          </b-row>
+          <b-row style="font-size: 20pt; margin: 10% 0px">
+            <b-col lg="1"><i class="fas fa-shopping-cart fas-10x fa-2x"> </i></b-col>
+            <b-col lg="11">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+            </b-col>
+          </b-row>
+          <b-row style="font-size: 20pt; margin: 7% 0px">
+            <b-col lg="1"><i class="fas fa-shopping-cart fas-10x fa-2x"> </i></b-col>
+            <b-col lg="11">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+            </b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Features'
+}
+</script>
+
+<style scoped>
+
+</style>

9623231f79df3ce3faf7a6ab9ff4173458af860c
alijalal-sys <ajmabilal@gmail.com>
2020-03-23 13:10:03 +0100

    Adding Media queries

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,34 +1,75 @@
 <template>
   <div class="curved">
-    <div class="float-left ml-5 text-left" style="font-size: 30pt">
-      Your Better Chioce <br />
-      Always <br />
-      <button class="shop-now">
-        <i class="fas fa-shopping-cart" /><span class="ml-3">Shop Now</span>
-      </button>
-    </div>
+    <b-row>
+      <b-col lg="5" md="6" class='colp'>
+        <div
+          class="hero-para"
+        >
+          <strong class='hero-header'>Everything You Want!</strong> <br />
+          <p class="hero-text">
+            Go Deep through Our Website and Find Everything you are Looking For! <br />
+          </p> 
+          <button class="shop-now">
+            <i class="fas fa-shopping-cart" /><span class="ml-3">Shop Now</span>
+          </button>
+        </div>
+      </b-col>
+      <b-col lg="7" md="6">
+        <div class="div-img">
+          <img
+            class="hero-img"
+            src="../assets/img/hero.png"
+            alt="woman-holding-bags"
+          />
+        </div>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'Hero',
+  name: "Hero",
   components: {}
-}
+};
 </script>
 
 <style scoped>
+/* @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap'); */
+
+/* parent */
 .curved {
+  font-family: 'Roboto', sans-serif;
   position: relative;
   width: 100%;
-  height: 500px;
+  height: 300px;
   margin-top: 222px;
-  background-color: #8BE2BC;
+  background-color: #8be2bc;
   border-top-right-radius: 0% 100%;
-  border-top-left-radius: 100% 100% ;
+  border-top-left-radius: 100% 100%;
   text-align: center;
-  text-indent: .1em;
+  text-indent: 0.1em;
 }
+
+/* Children */
+.hero-para {
+  float: left;
+  text-align: left;
+  font-size: 24pt;
+  margin-left: 20%;
+  position: relative;
+  bottom: 24%;
+}
+
+.hero-header {
+  font-size: 34pt;
+}
+
+.hero-text {
+  font-size: 18pt;
+}
+
 .shop-now {
   margin: 20px 0px;
   font-size: 20pt;
@@ -37,5 +78,81 @@ export default {
   border: 3px solid #000;
   border-radius: 1.6rem;
   background-color: #fff;
+}
+
+.div-img {
+  position: relative;
+  bottom: 35.7%;
+  padding-top: 0em;
+  width: 700px;
+}
+
+.colp {
+  height: 600px;
+}
+
+.hero-img {
+  width: 700px;
+}
+
+ 
+@media screen and (max-width: 800px) {
+  .curved {
+    font-family: 'Roboto', sans-serif;
+    position: relative;
+    width: 100%;
+    height: 307px;
+    margin-top: 222px;
+    background-color: #8be2bc;
+    border-top-right-radius: 0% 100%;
+    border-top-left-radius: 100% 100%;
+    text-align: center;
+    text-indent: 0.1em;
+  }
+  
+  .colp {
+    text-align: left;
+    height: 0px !important;
+  }
+
+  .hero-img {
+    width: 440px;
+  }
+  
+  .div-img {
+    position: relative;
+    bottom: 5%;
+    right: 4.4em;
+    padding-top: 0em;
+    width: 700px;
+  }
+
+  .hero-para {
+    float: left;
+    text-align: left;
+    font-size: 24pt;
+    position: relative;
+    bottom: 5.8em;
+    left: -2em;
+    width: 90%;
+  }
+
+  .hero-header {
+    font-size: 24pt;
+  }
+
+  .hero-text {
+    font-size: 16pt;
+  }
+
+  .shop-now {
+    margin: 8px 0;
+    font-size: 16pt;
+    word-spacing: .2px;
+    padding: 4px 26px !important;
+    border: 3px solid #000;
+    border-radius: 1.6rem;
+    background-color: #fff;
+  }
 }
 </style>

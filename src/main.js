94019f18bb2945ab18bd7b9ab815a7d6ff4e7aa6
alijalal-sys <ajmabilal@gmail.com>
2020-03-26 20:26:57 +0100

    setting up axios globally

--- src/main.js
+++ src/main.js
@@ -1,16 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import router from "./router/index";
-import $ from 'jquery'
+import Axios from 'axios';
+// import $ from 'jquery'
 import { BootstrapVue } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
+// import NavBar from './components/NavBar'
+// import Footer from './components/Footer'
+import '../globalComponents'
 
 Vue.use(BootstrapVue)
-Vue.use($)
+// Vue.use($)
 
+// Vue.component('NavBar', NavBar);
+// Vue.component('Footer', Footer);
+Vue.prototype.$http = Axios;
 Vue.config.productionTip = false
 
 new Vue({

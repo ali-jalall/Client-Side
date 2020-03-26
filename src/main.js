204dad66902da32de9202c13dfb1e584418fa5cb
alijalal-sys <ajmabilal@gmail.com>
2020-03-26 18:01:59 +0100

    adding jquery

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from "./router/index";
+import $ from 'jquery'
 import { BootstrapVue } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
@@ -8,6 +9,7 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 
 Vue.use(BootstrapVue)
+Vue.use($)
 
 Vue.config.productionTip = false
 

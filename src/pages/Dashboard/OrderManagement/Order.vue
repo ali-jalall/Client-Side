a1ace0cce11e00b39dc3bb13ec3db6ade0be3c3e
alijalal-sys <ajmabilal@gmail.com>
2020-04-14 17:02:22 +0200

    add single order page

--- src/pages/Dashboard/OrderManagement/Order.vue
+++ src/pages/Dashboard/OrderManagement/Order.vue
@@ -1,10 +1,185 @@
 <template>
   <div>
-    fds
+    <Widget
+      title="<h5 class='pt-3'><span class='fw-semi-bold '>Order</span></h5>"
+      bodyClass="widget-table-overflow py-2"
+      customHeader
+    >
+      <div class=" row">
+        <div class="col-md-11 m-auto text-right">
+          <b-button @click="removeOrder" :accesskey="order._id" variant="danger" class="ml-3 p-2 px-3">
+            X
+          </b-button>
+          <br />
+          <strong style="font-size: 14pt;">Status</strong>
+          
+          <b-button
+            :variant="
+              order.status === 'Pending'
+                ? 'info'
+                : order.status === 'Completed'
+                ? 'success'
+                : 'danger'
+            "
+            class="ml-3 p-2 px-3"
+          >
+            {{ order.status }}
+          </b-button>
+        </div>
+      </div>
+      <div class="row m-auto text-center">
+        <div class="col-md-4 text-center user_img">
+          <div class="text-center p-2" style="font-size: 16pt;">
+            User <strong>Profile</strong>
+          </div>
+          <img
+            src="../../../assets/img/marie.jpg"
+            class="user_img"
+            alt="user_img"
+          />
+          <div class="text-center user_data mt-2 ">
+            <p>Username: {{ order.username }}</p>
+            <p>Email: {{ user.email }}</p>
+            <p>Phone Number: {{ user.phone_number }}</p>
+            <p>City: {{ user.city }}</p>
+            <p>Address: {{ user.address }}</p>
+          </div>
+        </div>
+
+        <div class="col-md-8 table-responsive">
+          <div class="text-center p-2 mt-3" style="font-size: 16pt;">
+            Order <strong> Products</strong>
+          </div>
+          <table class="table mb-0 requests-table">
+            <thead>
+              <tr class="text-muted">
+                <th>IMAGE</th>
+                <th>NAME</th>
+                <th>CATEGORY</th>
+                <th>PRICE</th>
+                <th>QUANTITY</th>
+                <th>ACTIONS</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(product, index) in products" :key="product._id">
+                <td>
+                  <img
+                    :src="product.product_imgs[0]"
+                    alt="product_img"
+                    width="100px"
+                  />
+                </td>
+                <td>{{ product.name }}</td>
+                <td>{{ product.category }}</td>
+
+                <td>$ {{ product.price }}</td>
+                <td>{{ order.quantities[index] }}</td>
+                <td>
+                  <b-button
+                    @click="removeProduct"
+                    variant="danger"
+                    :accesskey="product._id"
+                    class="mr-1 p-1 px-3"
+                  >
+                    <i class="fas fa-minus-circle"></i>
+                  </b-button>
+                </td>
+              </tr>
+              <tr>
+                <td style="font-size: 14pt;"><b>Total Price:</b></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td style="font-size: 14pt;" class="pl-3">
+                  <b>${{ order.total_price }}</b>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </Widget>
   </div>
 </template>
 <script>
+const API_GET = "http://localhost:5000/orders";
+
+
 export default {
-  name: 'Order'
-}
+  name: "Order",
+  data() {
+    return {
+      order: {},
+      user: {},
+      products: [],
+      quantities: []
+    };
+  },
+  methods: {
+    getUserByOrder() {
+      this.$http
+        .put(`${API_GET}/${this.$route.params.id}`)
+        .then(({ data }) => {
+          this.user = data
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    },
+    getProductsByOrder() {
+      this.$http
+        .post(`${API_GET}/${this.$route.params.id}`)
+        .then(({ data }) => {
+          this.products = data.products;
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    },
+    getOrder () {
+      this.$http
+      .get(`${API_GET}/${this.$route.params.id}`)
+      .then(({ data }) => {
+        this.order = data.order;
+        this.quantities = data.order.quantities
+        console.log(this.order);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    },
+    removeProduct(e) {
+      console.log(e.target.accessKey)
+      this.$http
+        .patch(`${API_GET}/${this.$route.params.id}`, { id: e.target.accessKey })
+        .then(({ data }) => {
+          this.$set(this.order, this.order, data.order)
+          console.log(data.order)
+        })
+        .catch((err) => console.log(err));
+    },
+    removeOrder(e) {
+      this.$http
+        .delete(`${API_GET}/${e.target.accessKey}`)
+        .then(() => {
+          this.$router.go(-1)
+        })
+        .catch((err) => console.log(err));
+    },
+  },
+  mounted() {
+    this.getOrder()
+    this.getProductsByOrder();
+    this.getUserByOrder();
+  }
+};
 </script>
+<style scoped>
+.user_img {
+  border-radius: 50%;
+  padding: 14px 32px;
+  width: 100%;
+}
+</style>

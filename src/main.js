e168d98f316983ec6a7fd85c70dd5bcf9b243ba6
alijalal-sys <ajmabilal@gmail.com>
2020-04-15 12:33:06 +0200

    clean code

--- src/main.js
+++ src/main.js
@@ -8,8 +8,7 @@ import Toasted from 'vue-toasted';
 import VueCookie from 'vue-cookie'
 import VueTouch from 'vue-touch'
 import { BootstrapVue } from 'bootstrap-vue';
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap-vue/dist/bootstrap-vue.css';
+
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 

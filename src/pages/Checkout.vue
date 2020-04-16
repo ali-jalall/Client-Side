002a983b22fd0c0eda7bd3f1efb6ef3e3377fc58
alijalal-sys <ajmabilal@gmail.com>
2020-04-16 11:16:45 +0200

    complete checkout proccess

--- src/pages/Checkout.vue
+++ src/pages/Checkout.vue
@@ -1,15 +1,76 @@
 <template>
   <div style="background-color: #F9FBFD">
     <NavBar />
+
+    <b-modal
+      id="locationModal"
+      title="Plesae Complete Your data"
+      ok-only
+      ok-variant="secondary"
+      ok-title="Cancel"
+      no-close-on-backdrop
+    >
+      <form @submit.prevent="submitData">
+        <div
+          class="form-group"
+          :class="$v.phone.$error ? 'has-danger' : 'has-success'"
+        >
+          <label for="phone_number">Phone Number</label>
+          <input
+            v-model.trim="$v.phone.$model"
+            ref="phone"
+            class="form-control bg-white"
+            :class="{ 'form-control-danger': $v.phone.$error }"
+            id="phone_number"
+            type="text"
+          />
+          <br />
+        </div>
+        <div
+          class="form-group"
+          :class="$v.city.$error ? 'has-danger' : 'has-success'"
+        >
+          <label for="city">City</label>
+          <input
+            v-model.trim="$v.city.$model"
+            ref="city"
+            class="form-control bg-white"
+            :class="{ 'form-control-danger': $v.city.$error }"
+            id="city"
+            type="text"
+          />
+          <br />
+        </div>
+        <div
+          class="form-group"
+          :class="$v.address.$error ? 'has-danger' : 'has-success'"
+        >
+          <label for="address">Address</label>
+          <input
+            v-model.trim="$v.address.$model"
+            ref="address"
+            class="form-control bg-white"
+            :class="{ 'form-control-danger': $v.address.$error }"
+            id="address"
+            type="text"
+          />
+          <br />
+        </div>
+        <button type="submit" class="btn btn-primary btn-block">
+          Submit
+        </button>
+      </form>
+    </b-modal>
+
     <div class="page-header page-header-small image">
       <div class="page-header-image" data-parallax="true"></div>
       <div class="content-center">
         <div class="container">
           <h1 class="title">This is our great company.</h1>
           <div class="text-center">
-            <a href="#pablo" class="btn btn-primary btn-icon btn-round"> </a>
-            <a href="#pablo" class="btn btn-primary btn-icon btn-round"> </a>
-            <a href="#pablo" class="btn btn-primary btn-icon btn-round"> </a>
+            <a href="#" class="btn btn-primary btn-icon btn-round"> </a>
+            <a href="#" class="btn btn-primary btn-icon btn-round"> </a>
+            <a href="#" class="btn btn-primary btn-icon btn-round"> </a>
           </div>
         </div>
       </div>
@@ -27,8 +88,6 @@
                 <th>Product Name</th>
                 <th>Product Price</th>
                 <th>Product Quantity</th>
-                <!-- <th>ADDRESS</th> -->
-                <!-- <th>CITY</th> -->
                 <th>ACTIONS</th>
               </tr>
             </thead>
@@ -83,19 +142,42 @@
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import { mapGetters, mapActions } from "vuex";
+import { required, minLength } from "vuelidate/lib/validators";
 
-const API_POST = "https://tranquil-everglades-67262.herokuapp.com/orders";
+const API_POST = "http://localhost:5000/orders";
+const API_GET = "http://localhost:5000/users";
 
 export default {
   name: "Checkout",
   components: {
     NavBar,
-    Footer
+    Footer,
+  },
+  data() {
+    return {
+      phone: "",
+      city: "",
+      address: "",
+    };
+  },
+  validations: {
+    phone: {
+      required,
+      minLength: minLength(8),
+    },
+    city: {
+      required,
+      minLength: minLength(6),
+    },
+    address: {
+      required,
+      minLength: minLength(16),
+    },
   },
   computed: {
     ...mapGetters({
       products: "cartProducts",
-      length: "getNumberOfCartProducts"
+      length: "getNumberOfCartProducts",
     }),
     total() {
       return this.products.reduce((total, p) => {
@@ -103,15 +185,15 @@ export default {
       }, 0);
     },
     products_ids() {
-      return this.products.map(product => {
+      return this.products.map((product) => {
         return product._id;
       });
     },
-    quantities () {
-      return this.products.map(product => {
-        return product.quantity
-      })
-    }
+    quantities() {
+      return this.products.map((product) => {
+        return product.quantity;
+      });
+    },
   },
   mounted() {
     console.log(this.products);
@@ -121,40 +203,100 @@ export default {
     removeProductById(e) {
       this.removeProduct(e.target.accessKey);
     },
-    orderNow() {
-      const username = this.$cookie.get("Username");
-      const user_id = this.$cookie.get("user_id");
-      const products_ids = this.products_ids;
-      const quantities = this.quantities;
-      const total_price = this.total;
+    async submitData() {
+      try {
+        const user_id = this.$cookie.get("user_id");
+        const products_ids = this.products_ids;
+        const quantities = this.quantities;
+        const total_price = this.total;
 
-      this.$http
-        .post(API_POST + "/add", {
-          username,
+        // Validate data entered
+        this.$v.$touch();
+        // if !Valid ? return
+        if (this.$v.$error) return;
+        // We Assume that user changes his data eg: address, city, phone_number
+        // We send request to update user
+        const {
+          data: { userupdated },
+        } = await this.$http.patch(`${API_GET}/${user_id}`, {
+          phone_number: this.phone,
+          city: this.city,
+          address: this.address,
+        });
+
+        // when updated we send request to create order
+        const order = await this.$http.post(API_POST + "/add", {
+          username: userupdated.username,
           user_id,
           products_ids,
           total_price,
-          quantities
-        })
-        .then(() => {
-          this.resetState();
-          // this.$router.push({ path: '/products' })
-          /**
-           * TODO: Show model to confirm sending the order and redirect to products page || Home
-           */
-        })
-        .then(err => {
-          /**
-           * TODO: Show model to decline order with err
-           */
-          console.log(err);
+          quantities,
         });
-    }
-  }
+
+        this.$toasted.success("Your Order has been sent", {
+          duration: 3000,
+          position: "top-center",
+          action: {
+            text: "Ok",
+            onComplete: (e, toastObject) => {
+              toastObject.goAway(0);
+            },
+          },
+        });
+
+        this.$bvModal.hide("locationModal");
+        this.resetState();
+        setTimeout(() => {
+          this.$router.push({ path: "products" });
+        }, 3000);
+
+        console.log(order);
+      } catch (e) {
+        this.$toasted.error("Sorry it seems like there's an issue!", {
+          duration: 3000,
+          position: "top-center",
+        });
+      }
+    },
+    orderNow() {
+      const user_id = this.$cookie.get("user_id");
+
+      this.$bvModal.show("locationModal");
+      this.$http
+        .get(`${API_GET}/${user_id}`)
+        .then(({ data }) => {
+          this.phone = data.user.phone_number;
+          this.city = data.user.city;
+          this.address = data.user.address;
+        })
+        .catch(() => {
+          this.$toasted.error("Sorry it seems like there's an issue!", {
+            position: "top-center",
+          });
+        });
+    },
+  },
 };
 </script>
 <style scoped>
 .image {
   background-image: url("../assets/img/bg26.jpg");
+}
+
+.has-danger:after {
+  content: "\ea53";
+  color: #ff3636;
+  padding-top: 30px;
+}
+.has-success:after {
+  /* content: "\ea53"; */
+  /* color: #ff3636; */
+  padding-top: 30px;
+}
+
+.has-danger .form-control {
+  border-color: #f67777;
+  color: #ff3636;
+  background-color: hsla(0, 0%, 87%, 0.1);
 }
 </style>

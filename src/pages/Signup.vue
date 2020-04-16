74016335a57c834670093fbc5b371553a5b8822a
alijalal-sys <ajmabilal@gmail.com>
2020-04-16 16:55:37 +0200

    add validator in Front

--- src/pages/Signup.vue
+++ src/pages/Signup.vue
@@ -68,77 +68,108 @@
                   >
                     {{ errorMessage }}
                   </b-alert>
-                  <form class="form" @submit.prevent="submitForm">
-                    <div class="input-group">
-                      <div class="input-group-prepend">
-                        <span class="input-group-text"
-                          ><i class="now-ui-icons users_circle-08"></i
-                        ></span>
-                      </div>
+                  <form class="form text-left" @submit.prevent="submitForm">
+                    <div
+                      class="form-group"
+                      :class="$v.user.username.$error ? 'has-danger' : ''"
+                    >
+                      <label for="username">Username</label>
                       <input
-                        required
                         v-model="user.username"
+                        class="form-control"
+                        id="username"
                         type="text"
-                        class="form-control"
-                        placeholder="Username..."
                       />
-                    </div>
-                    <div class="input-group">
-                      <div class="input-group-prepend">
-                        <span class="input-group-text"
-                          ><i class="now-ui-icons ui-1_email-85"></i
-                        ></span>
+                      <div
+                        class="error text-danger"
+                        v-if="!$v.user.username.minLength"
+                      >
+                        Name must have at least
+                        {{ $v.user.username.$params.minLength.min }} letters.
                       </div>
+                      <br />
+                    </div>
+                    <div
+                      class="form-group"
+                      :class="$v.user.email.$error ? 'has-danger' : ''"
+                    >
+                      <label for="email">Email</label>
                       <input
-                        required
                         v-model="user.email"
+                        class="form-control"
+                        id="email"
                         type="email"
-                        class="form-control"
-                        placeholder="Email..."
                       />
-                    </div>
+                      <div
+                        class="error text-danger"
+                        v-if="!$v.user.email.email"
+                      >
+                        Must be an Email
+                      </div>
 
-                    <div class="input-group">
-                      <div class="input-group-prepend">
-                        <span class="input-group-text"
-                          ><i class="now-ui-icons text_caps-small"></i
-                        ></span>
-                      </div>
+                      <br />
+                    </div>
+                    <div
+                      class="form-group"
+                      :class="$v.user.password.$error ? 'has-danger' : ''"
+                    >
+                      <label for="password">Password</label>
                       <input
-                        required
                         v-model="user.password"
+                        class="form-control"
+                        id="password"
                         type="password"
-                        class="form-control"
-                        placeholder="Password..."
                       />
-                    </div>
-                    <div class="input-group">
-                      <div class="input-group-prepend">
-                        <span class="input-group-text"
-                          ><i class="now-ui-icons text_caps-small"></i
-                        ></span>
+                      <div
+                        class="error text-danger"
+                        v-if="!$v.user.password.minLength"
+                      >
+                        Password must have at least
+                        {{ $v.user.password.$params.minLength.min }} letters.
                       </div>
+                      <br />
+                    </div>
+                    <div
+                      class="form-group"
+                      :class="$v.user.age.$error ? 'has-danger' : ''"
+                    >
+                      <label for="age">Age</label>
                       <input
-                        required
                         v-model="user.age"
-                        type="number"
                         class="form-control"
-                        placeholder="Age..."
+                        id="age"
+                        type="number"
                       />
-                    </div>
-                    <div class="input-group">
-                      <div class="input-group-prepend">
-                        <span class="input-group-text"
-                          ><i class="now-ui-icons text_caps-small"></i
-                        ></span>
+                      <div
+                        class="error text-danger"
+                        v-if="!$v.user.age.between"
+                      >
+                        Please Enter Valid Age.
                       </div>
+                      <br />
+                    </div>
+                    <div
+                      class="form-group"
+                      :class="$v.user.phone_number.$error ? 'has-danger' : ''"
+                    >
+                      <label for="phone_number">Phone Number</label>
                       <input
                         v-model="user.phone_number"
-                        type="number"
                         class="form-control"
-                        placeholder="Phone Number..."
+                        id="phone_number"
+                        type="number"
                       />
+                      <div
+                        class="error text-danger"
+                        v-if="!$v.user.phone_number.minLength"
+                      >
+                        Phone Number must have at least
+                        {{ $v.user.phone_number.$params.minLength.min }}
+                        Numbers.
+                      </div>
+                      <br />
                     </div>
+
                     <input
                       class="btn btn-primary"
                       variant="primary"
@@ -158,7 +189,9 @@
 </template>
 
 <script>
-import Footer from "../components/Footer";
+import Footer from "@/components/Footer";
+import { required, minLength, email, between } from "vuelidate/lib/validators";
+
 const API_SIGNUP = "http://localhost:5000/users/add";
 
 export default {
@@ -176,34 +209,58 @@ export default {
       },
     };
   },
+  validations: {
+    user: {
+      username: {
+        required,
+        minLength: minLength(6),
+      },
+      email: {
+        required,
+        email,
+      },
+      password: {
+        required,
+        minLength: minLength(8),
+      },
+      age: {
+        required,
+        between: between(1, 100),
+      },
+      phone_number: {
+        required,
+        minLength: minLength(10),
+      },
+    },
+  },
   methods: {
     submitForm() {
+      this.$v.user.$touch();
+      if (this.$v.user.$error) return;
       let loader = this.$loading.show({
-        // Optional parameters
         container: this.fullPage ? null : this.$refs.productContainer,
       });
+
       this.$http
         .post(API_SIGNUP, this.user)
         .then(({ data }) => {
-          console.log(data);
-          if (data.errMsg) {
-            if (data.errMsg.includes("Must")) {
-              loader.hide();
-              this.errorMessage = data.errMsg;
-            } else {
-              loader.hide();
-              // TODO: show error notification
-            }
-          }
           loader.hide();
-          this.$cookie.set("X-auth", data.token);
-          this.$cookie.set("auth", true);
-          this.$cookie.set("Username", data.username);
-          this.$cookie.set("user_id", data.user_id);
+          if (data.auth) {
+            this.$cookie.set("X-auth", data.token);
+            this.$cookie.set("auth", true);
+            this.$cookie.set("Username", data.username);
+            this.$cookie.set("user_id", data.user_id);
+            this.$router.push("/");
+          } else {
+            this.$toasted.error("Sorry it seems like there's an issue!", {
+              duration: 3000,
+              position: "top-center",
+            });
+          }
 
-          this.$router.push("/");
         })
         .catch(() => {
+          loader.hide();
           this.$toasted.error("Sorry it seems like there's an issue!", {
             duration: 3000,
             position: "top-center",
@@ -236,5 +293,35 @@ export default {
   margin-top: 2%;
   text-align: center;
   margin-bottom: -90px;
+}
+
+.has-danger:after {
+  content: "\ea53";
+  color: #ff3636;
+  padding-top: 27px;
+}
+.has-success:after {
+  padding-top: 30px;
+}
+
+.has-danger .form-control {
+  border-color: #f67777;
+  color: #ff3636;
+  background-color: hsla(0, 0%, 87%, 0.1);
+}
+
+.signup-page .form-group,
+.signup-page .input-group {
+  margin-bottom: -10px;
+}
+
+label {
+  color: rgb(112, 105, 105);
+  margin-left: 10px;
+}
+
+.error {
+  font-size: 10pt;
+  padding-left: 6px;
 }
 </style>
